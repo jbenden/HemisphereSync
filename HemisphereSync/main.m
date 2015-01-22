3ddb0ad58fded8900169a4bb007f9fc0062b76be
Joseph Benden <joe@thrallingpenguin.com>
2015-01-22 10:23:51 -0700

    Initial Commit

---
+++ HemisphereSync/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HemisphereSync
+//
+//  Created by Joseph Benden on 1/22/15.
+//  Copyright (c) 2015 Joseph Benden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
